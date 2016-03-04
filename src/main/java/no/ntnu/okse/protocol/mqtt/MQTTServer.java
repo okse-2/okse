d9ad2cb57a46ca3af2d645ac17508a534ea74745
Gabriel Berthling-Hansen <gabrielb@stud.ntnu.no>
2016-03-04 16:43:21 +0100

    Whenever a message is sent to Moquette it will send that message to OKSE core, it will also send messages from OKSE core to any subscribers

--- src/main/java/no/ntnu/okse/protocol/mqtt/MQTTServer.java
+++ src/main/java/no/ntnu/okse/protocol/mqtt/MQTTServer.java
@@ -4,32 +4,63 @@ import io.moquette.BrokerConstants;
 import io.moquette.interception.AbstractInterceptHandler;
 import io.moquette.interception.InterceptHandler;
 import io.moquette.interception.messages.InterceptPublishMessage;
+import io.moquette.interception.messages.InterceptSubscribeMessage;
+import io.moquette.proto.messages.AbstractMessage;
+import io.moquette.proto.messages.PublishMessage;
 import io.moquette.server.Server;
-import io.moquette.server.config.FilesystemConfig;
 import io.moquette.server.config.IConfig;
 import io.moquette.server.config.MemoryConfig;
+import io.moquette.spi.ClientSession;
 import no.ntnu.okse.core.messaging.Message;
+import no.ntnu.okse.core.messaging.MessageService;
+import no.ntnu.okse.core.subscription.Publisher;
+import no.ntnu.okse.core.subscription.Subscriber;
+import no.ntnu.okse.core.subscription.SubscriptionService;
+import no.ntnu.okse.core.topic.TopicService;
 import org.apache.log4j.Logger;
 
+import io.moquette.spi.impl.ProtocolProcessor;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.nio.ByteBuffer;
+import java.util.*;
 
 public class MQTTServer extends Server {
 
-	private static Logger logger = Logger.getLogger(Server.class);
+	private static Logger log = Logger.getLogger(Server.class);
+	private static String protocolServerType;
 
-	static class MQTTListener extends AbstractInterceptHandler {
+	class MQTTListener extends AbstractInterceptHandler {
 		@Override
 		public void onPublish(InterceptPublishMessage message) {
-			logger.info("Received message on topic " + message.getTopicName());
+			//TODO: We need to get the publisher that sent the message, somehow
+			Publisher pub = new Publisher( message.getTopicName(), "127.0.0.1", 1883, protocolServerType);
+			ByteBuffer buffer = message.getPayload();
+			String payload = new String(buffer.array(), buffer.position(), buffer.limit());
+			String topic = message.getTopicName();
+			MessageService.getInstance().distributeMessage(
+					new Message( payload, topic, pub, protocolServerType )
+			);
 		}
+
+		@Override
+		public void onSubscribe(InterceptSubscribeMessage message) {
+			log.info("Client subscribed to: "  + message.getTopicFilter() + "   ID: " + message.getClientID());
+
+			TopicService.getInstance().addTopic( message.getTopicFilter() );
+			Subscriber sub = new Subscriber( "127.0.0.1", 1309, message.getTopicFilter(), protocolServerType );
+			SubscriptionService.getInstance().addSubscriber(sub);
+
+			super.onSubscribe(message);
+		}
+
 	}
 
 	public void init(String host, int port) {
 		List<InterceptHandler> interceptHandlers = new ArrayList<>();
 		interceptHandlers.add(new MQTTListener());
+
+		protocolServerType = "MQTT";
 
 		Properties properties = new Properties();
 		properties.setProperty(BrokerConstants.HOST_PROPERTY_NAME, host);
@@ -45,7 +76,23 @@ public class MQTTServer extends Server {
 
 	}
 
+	/**
+	 * Sends the message to any subscriber that is subscribed to the topic that the message was sent to
+	 * @param message is the message that is sent from OKSE core
+	 * */
 	public void sendMessage(Message message) {
+		HashSet<Subscriber> subscribers = SubscriptionService.getInstance().getAllSubscribersForTopic( message.getTopic() );
+		for(Subscriber subscriber : subscribers){
+			PublishMessage msg = new PublishMessage();
+			ByteBuffer payload = ByteBuffer.wrap( message.getMessage().getBytes() );
+			String topicName = message.getTopic();
+
+			msg.setPayload( payload );
+			msg.setTopicName( topicName );
+
+			msg.setQos(AbstractMessage.QOSType.LEAST_ONE);
+			internalPublish(msg);
+		}
 
 	}
 }
