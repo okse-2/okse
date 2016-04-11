7a5b8fe4e42532f7de1d61bdfedbd845c1091e3e
Christian Duvholt <christdu@stud.ntnu.no>
2016-04-11 15:38:37 +0200

    Add some more tests for missing branches

--- src/test/java/no/ntnu/okse/protocol/amqp091/AMQP091ProtocolServerTest.java
+++ src/test/java/no/ntnu/okse/protocol/amqp091/AMQP091ProtocolServerTest.java
@@ -1,12 +1,17 @@
 package no.ntnu.okse.protocol.amqp091;
 
 import no.ntnu.okse.core.messaging.Message;
+import no.ntnu.okse.core.subscription.Subscriber;
 import no.ntnu.okse.core.subscription.SubscriptionService;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 import static org.testng.AssertJUnit.*;
@@ -45,6 +50,15 @@ public class AMQP091ProtocolServerTest {
         assertTrue(protocolServer.isRunning());
     }
 
+    public void boot_twice() {
+        protocolServer.setAmqpService(null);
+        doNothing().when(protocolServer).run();
+        protocolServer.boot();
+        assertTrue(protocolServer.isRunning());
+        protocolServer.boot();
+        assertTrue(protocolServer.isRunning());
+    }
+
     public void run_stop() {
         protocolServer.run();
         verify(amqpService).start();
@@ -70,7 +84,18 @@ public class AMQP091ProtocolServerTest {
 
     public void sendMessage_Subscribers() {
         Message message = new Message("Message body", "topic", null, protocolServer.getProtocolServerType());
+        int numberOfTopicSubscribers = 10;
+        HashSet<Subscriber> subscribers = new HashSet<>();
+        for(int i = 0; i < numberOfTopicSubscribers; i++) {
+            subscribers.add(createSubscriber("topic"));
+            subscribers.add(createSubscriber("notTopic"));
+        }
+        doReturn(subscribers).when(subscriptionService).getAllSubscribers();
         protocolServer.sendMessage(message);
-        verify(protocolServer, never()).incrementTotalMessagesSent();
+        verify(protocolServer, times(numberOfTopicSubscribers)).incrementTotalMessagesSent();
+    }
+
+    private Subscriber createSubscriber(String topic) {
+        return new Subscriber("localhost", 1234, topic, protocolServer.getProtocolServerType());
     }
 }
