7dab84ba273f608dab6ac5eb3b8b9ad2a8b86c96
andreas weisethaunet <andreas weisethaunet>
2016-04-28 15:00:17 +0200

    added suport to change port++

--- src/main/java/no/ntnu/okse/protocol/xmpp/XMPPProtocolServer.java
+++ src/main/java/no/ntnu/okse/protocol/xmpp/XMPPProtocolServer.java
@@ -1,25 +1,53 @@
 package no.ntnu.okse.protocol.xmpp;
 
 
+import java.io.IOException;
+import java.net.SocketAddress;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import no.ntnu.okse.core.messaging.Message;
 import no.ntnu.okse.protocol.AbstractProtocolServer;
+import org.apache.mina.core.filterchain.DefaultIoFilterChainBuilder;
+import org.apache.mina.core.filterchain.IoFilterChain;
+import org.apache.mina.core.filterchain.IoFilterChainBuilder;
+import org.apache.mina.core.filterchain.IoFilterEvent;
+import org.apache.mina.core.future.WriteFuture;
+import org.apache.mina.core.service.*;
+import org.apache.mina.core.session.IoEvent;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.core.session.IoSessionConfig;
+import org.apache.mina.core.session.IoSessionDataStructureFactory;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.util.CommonEventFilter;
 import org.apache.vysper.mina.S2SEndpoint;
 import org.apache.vysper.mina.TCPEndpoint;
+import org.apache.vysper.mina.codec.StanzaWriteInfo;
 import org.apache.vysper.storage.OpenStorageProviderRegistry;
 import org.apache.vysper.storage.StorageProviderRegistry;
+import org.apache.vysper.xmpp.addressing.Entity;
+import org.apache.vysper.xmpp.addressing.EntityImpl;
 import org.apache.vysper.xmpp.modules.extension.xep0049_privatedata.PrivateDataModule;
+import org.apache.vysper.xmpp.modules.extension.xep0050_adhoc_commands.AdhocCommandsModule;
 import org.apache.vysper.xmpp.modules.extension.xep0054_vcardtemp.VcardTempModule;
 import org.apache.vysper.xmpp.modules.extension.xep0060_pubsub.PublishSubscribeModule;
 import org.apache.vysper.xmpp.modules.extension.xep0092_software_version.SoftwareVersionModule;
 import org.apache.vysper.xmpp.modules.extension.xep0119_xmppping.XmppPingModule;
 import org.apache.vysper.xmpp.modules.extension.xep0202_entity_time.EntityTimeModule;
 import org.apache.vysper.xmpp.modules.roster.persistence.MemoryRosterManager;
+import org.apache.vysper.xmpp.modules.servicediscovery.management.ItemRequestListener;
 import org.apache.vysper.xmpp.server.ServerFeatures;
 import org.apache.vysper.xmpp.server.XMPPServer;
+import org.apache.vysper.xmpp.stanza.Stanza;
+import org.apache.vysper.xmpp.stanza.StanzaBuilder;
 
 public class XMPPProtocolServer extends AbstractProtocolServer {
+    private TCPEndpoint tcpEndpoint;
     protected static final String SERVERTYPE = "xmpp";
 
     private static Logger log = Logger.getLogger(XMPPProtocolServer.class.getName());
@@ -34,22 +62,20 @@ public class XMPPProtocolServer extends AbstractProtocolServer {
 
         StorageProviderRegistry providerRegistry = new OpenStorageProviderRegistry();
         providerRegistry.add(new Authorization());
+
         providerRegistry.add(new MemoryRosterManager());
-
         final String pathToTLSCertificate = "/keystore.jks";
-
         XMPPServer server = new XMPPServer(domain);
 
         // allow XMPP federation
         server.addEndpoint(new S2SEndpoint());
         // enable classic TCP bases access
-        server.addEndpoint(new TCPEndpoint());
-
-
-
-
+        tcpEndpoint = new TCPEndpoint();
+        tcpEndpoint.setPort(port);
+        tcpEndpoint
+        log.info("XMPP port it set to " + port);
+        server.addEndpoint(tcpEndpoint);
         server.setStorageProviderRegistry(providerRegistry);
-
         server.setTLSCertificateInfo(XMPPProtocolServer.class.getResourceAsStream(pathToTLSCertificate), "password1");
 
         try {
@@ -59,12 +85,47 @@ public class XMPPProtocolServer extends AbstractProtocolServer {
             throw new RuntimeException(e);
         }
 
+        ProtocolCodecFilter codecFilter = new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" )));
+        //tcpEndpoint.getFilterChainBuilder().addLast( "codec", codecFilter);
+
         server.addModule(new SoftwareVersionModule());
         server.addModule(new EntityTimeModule());
         server.addModule(new VcardTempModule());
         server.addModule(new XmppPingModule());
         server.addModule(new PrivateDataModule());
-        server.addModule(new PublishSubscribeModule());
+        AdhocCommandsModule adhocCommandsModule = new AdhocCommandsModule();
+        server.addModule(adhocCommandsModule);
+
+
+        PublishSubscribeModule  publishSubscribeModule = new PublishSubscribeModule();
+        server.addModule(publishSubscribeModule);
+        publishSubscribeModule
+        //the line under sets relaying messages on/off
+        //server.getServerRuntimeContext().getServerFeatures().setRelayingMessages();
+        //IoEvent event = new IoEvent()
+
+        DefaultIoFilterChainBuilder filterChainBuilder = tcpEndpoint.getFilterChainBuilder();
+
+        StanzaBuilder stanzaBuilder = new StanzaBuilder("fucku");
+        stanzaBuilder.addAttribute("aa", "ad");
+
+        System.out.println("aaaaa" + server.getServerRuntimeContext().getResourceRegistry().getSessionCount());
+        //filterChainBuilder.addAfter("sss", );
+//        List<IoFilterChain.Entry> entries = filterChainBuilder.getAll();
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        System.out.println("printing out size " + entries.size());
+//        for(int i = 0; i < entries.size(); i++){
+//            System.out.println(entries.toString());
+//        }
+
+
+
+
     }
 
     @Override
