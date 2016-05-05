1567416a347b01c87fd0e90ee989a22bc5b6241c
Christian Duvholt <christdu@stud.ntnu.no>
2016-05-05 23:15:08 +0200

    Increment MQTT error

--- src/main/java/no/ntnu/okse/protocol/mqtt/MQTTServer.java
+++ src/main/java/no/ntnu/okse/protocol/mqtt/MQTTServer.java
@@ -82,6 +82,7 @@ public class MQTTServer extends Server {
         try {
             startServer(config, interceptHandlers);
         } catch (IOException e) {
+            ps.incrementTotalErrors();
             e.printStackTrace();
         }
     }
